d6e836257e9011661847572578126bf3660b1085
Katya6589 <113089569+Katya6589@users.noreply.github.com>
2023-12-23 12:08:24 +0300

    Add files via upload

---
+++ Barsegyan_EXAM/Barsegyan_EXAM/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Barsegyan_EXAM
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Class1._mainFrame = _mainFrame;
+            Class1._mainFrame.Navigate(new pages.Page1());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new pages.Page2());
+        }
+
+        private void Button_Nazad(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new pages.Page1());
+        }
+
+        private void Button_Text(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new pages.Page3());
+        }
+
+    }
+}
